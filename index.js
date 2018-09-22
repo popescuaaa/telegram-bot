c1e96dcd72fc29407899f037bfc8a3e99930b3fa
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2018-09-22 19:29:10 +0300

    Weather function

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 'use strict'
 
 const TelegramBot = require('node-telegram-bot-api'); // library import in Node.js
+const Weather = require('weather-js'); 
 
 const token = '578751631:AAGbBFePFXtg6Yng5vp33iipI1o0tlKJFro';
 
+var weather_forecast;
+var location;
+var wetaher;
+
 
 const bot = new TelegramBot(token, {polling: true});
+
+Weather.find({search: 'Bucharest, RO', degreeType: 'C'}, function(err, result) {
+    if(err) console.log(err);
+   
+    weather_forecast = JSON.parse(JSON.stringify(result, null, 2));
+
+    
+});
+
+
+
 
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -19,23 +35,23 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 });
 
 
-
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-    var hi = "hi";
-    var bye = "bye";
-    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
-    bot.sendMessage(msg.chat.id,"Hello dear user");
+    var commands =['hi','bye','weather'];
+    if (msg.text.toString().toLowerCase().indexOf(commands[0]) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user"); 
+        // here is a perfect match with the message 'hi'
     } 
-    else if (msg.text.toString().toLowerCase().includes(bye)) {
+    if (msg.text.toString().toLowerCase().includes(commands[1])) {
     bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-    } else {
-        
+        // but here, if the message contains 'bye'  
     }
-
+    if (msg.text.toString().toLowerCase().includes(commands[2])) {
+        bot.sendMessage(msg.chat.id,weather_forecast);
+    }
+    
 
 });
-
 
 
