ee96b44e725a3d8d40373e23b7cf69f5276eb492
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2018-09-23 18:33:17 +0300

    Fix Weather

--- index.js
+++ index.js
@@ -5,53 +5,45 @@ const Weather = require('weather-js');
 
 const token = '578751631:AAGbBFePFXtg6Yng5vp33iipI1o0tlKJFro';
 
-var weather_forecast;
-var location;
-var wetaher;
+var weather_mood;
+var city;
+var temperature;
+var we
 
 
 const bot = new TelegramBot(token, {polling: true});
 
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1]; 
+    bot.sendMessage(chatId, resp);
+  });
+
 Weather.find({search: 'Bucharest, RO', degreeType: 'C'}, function(err, result) {
     if(err) console.log(err);
+    city = result[0].location.name;
+    temperature = result[0].current.temperature;
+    weather_mood =  result[0].current.skytext;   
    
-    weather_forecast = JSON.parse(JSON.stringify(result, null, 2));
-
-    
 });
-
-
-
-
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; 
-
- 
-  bot.sendMessage(chatId, resp);
-});
-
 
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-    var commands =['hi','bye','weather'];
-    if (msg.text.toString().toLowerCase().indexOf(commands[0]) === 0) {
-    bot.sendMessage(msg.chat.id,"Hello dear user"); 
-        // here is a perfect match with the message 'hi'
-    } 
-    if (msg.text.toString().toLowerCase().includes(commands[1])) {
-    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-        // but here, if the message contains 'bye'  
-    }
-    if (msg.text.toString().toLowerCase().includes(commands[2])) {
-        bot.sendMessage(msg.chat.id,weather_forecast);
-    }
-    
-
-});
-
-
+    const chatId = msg.chat.id;
+  
+      var commands =['hi','bye','weather'];
+      if (msg.text.toString().toLowerCase().indexOf(commands[0]) === 0) {
+      bot.sendMessage(msg.chat.id,"Hello dear user"); 
+          // here is a perfect match with the message 'hi'
+      } 
+      if (msg.text.toString().toLowerCase().includes(commands[1])) {
+      bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+          // but here, if the message contains 'bye'  
+      }
+      if (msg.text.toString().toLowerCase().includes(commands[2])) {
+          bot.sendMessage(msg.chat.id,"The weather is: " + weather_mood + "\n"
+                            +"The current temperature is: " + temperature );
+  
+      }
+      
+  
+  });
